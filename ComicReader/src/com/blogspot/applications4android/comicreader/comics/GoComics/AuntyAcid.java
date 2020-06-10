2e2fbbcb85e11380fd3d45600090a5c0cd198794
scott snyder <sss@karma>
2020-06-10 00:06:05 -0400

    Some new strips from gocomics:
    adult-children
    the-adventures-of-business-cat
    aj-and-magnus
    alis-house
    amanda-the-great
    american-chop-suey
    angry-birds
    ask-a-cat
    ask-a-portly-syndicate-person
    aunty-acid

---
+++ ComicReader/src/com/blogspot/applications4android/comicreader/comics/GoComics/AuntyAcid.java
@@ -0,0 +1,13 @@
+package com.blogspot.applications4android.comicreader.comics.GoComics;
+
+import java.util.Calendar;
+import com.blogspot.applications4android.comicreader.comictypes.DailyGoComicsCom;
+
+public class AuntyAcid extends DailyGoComicsCom {
+	public AuntyAcid() {
+		super();
+		mComicName = "aunty-acid";
+		mFirstCal = Calendar.getInstance();
+		mFirstCal.set(2013, 4, 6);
+	}
+}
