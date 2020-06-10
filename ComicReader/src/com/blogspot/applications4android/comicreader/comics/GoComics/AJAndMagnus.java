7d8eefa0b6dd6d0fa1f5f7365f97c4d6b0485c9d
scott snyder <sss@karma>
2020-06-10 00:06:51 -0400

    Refresh of gonew1

---
+++ ComicReader/src/com/blogspot/applications4android/comicreader/comics/GoComics/AJAndMagnus.java
@@ -0,0 +1,13 @@
+package com.blogspot.applications4android.comicreader.comics.GoComics;
+
+import java.util.Calendar;
+import com.blogspot.applications4android.comicreader.comictypes.DailyGoComicsCom;
+
+public class AJAndMagnus extends DailyGoComicsCom {
+	public AJAndMagnus() {
+		super();
+		mComicName = "aj-and-magnus";
+		mFirstCal = Calendar.getInstance();
+		mFirstCal.set(2014, 7, 8);
+	}
+}
