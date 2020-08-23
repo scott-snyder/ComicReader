96560d0734dfad65f15eac63aac0de9bca408870
scott snyder <sss@karma>
2020-08-22 22:34:34 -0400

    More new strips from gocomics:
    the-awkward-yetoi
    back-to-bc
    bacon
    bad-machinery
    batch-rejection
    beanie-the-browie
    bear-with-me
    the-bent-pinky
    berkeley-mews
    the-best-medicine

---
+++ ComicReader/src/com/blogspot/applications4android/comicreader/comics/GoComics/BadMachinery.java
@@ -0,0 +1,13 @@
+package com.blogspot.applications4android.comicreader.comics.GoComics;
+
+import java.util.Calendar;
+import com.blogspot.applications4android.comicreader.comictypes.DailyGoComicsCom;
+
+public class BadMachinery extends DailyGoComicsCom {
+	public BadMachinery() {
+		super();
+		mComicName = "bad-machinery";
+		mFirstCal = Calendar.getInstance();
+		mFirstCal.set(2016, 3, 11);
+	}
+}
