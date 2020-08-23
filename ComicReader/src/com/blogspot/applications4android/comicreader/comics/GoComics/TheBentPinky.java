1f042d2ed6a1779b9acdf3a4f7552e173f73caea
scott snyder <sss@karma>
2020-08-22 22:34:34 -0400

    More new strips from gocomics:
    the-awkward-yetoi
    back-to-bc
    bacon
    bad-machinery
    batch-rejection
    beanie-the-browie
    bear-with-me
    the-bent-pinky
    berkeley-mews
    the-best-medicine

---
+++ ComicReader/src/com/blogspot/applications4android/comicreader/comics/GoComics/TheBentPinky.java
@@ -0,0 +1,13 @@
+package com.blogspot.applications4android.comicreader.comics.GoComics;
+
+import java.util.Calendar;
+import com.blogspot.applications4android.comicreader.comictypes.DailyGoComicsCom;
+
+public class TheBentPinky extends DailyGoComicsCom {
+	public TheBentPinky() {
+		super();
+		mComicName = "the-bent-pinky";
+		mFirstCal = Calendar.getInstance();
+		mFirstCal.set(2013, 7, 26);
+	}
+}
---
+++ ComicReader/src/com/blogspot/applications4android/comicreader/comics/GoComics/TheBestMedicine.java
@@ -0,0 +1,13 @@
+package com.blogspot.applications4android.comicreader.comics.GoComics;
+
+import java.util.Calendar;
+import com.blogspot.applications4android.comicreader.comictypes.DailyGoComicsCom;
+
+public class TheBestMedicine extends DailyGoComicsCom {
+	public TheBestMedicine() {
+		super();
+		mComicName = "the-best-medicine";
+		mFirstCal = Calendar.getInstance();
+		mFirstCal.set(2015, 8, 14);
+	}
+}
